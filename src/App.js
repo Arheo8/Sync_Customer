d783fad5988475783de48d4183e69c333ac87bed
Arheo8 <78500348+Arheo8@users.noreply.github.com>
2021-09-17 18:53:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from "react";
 import Button from "./components/button";
 
-const authenticateUrl = "https://api.thecodemesh.online/api/v1/enable/user-workflow/614393c4179d450012f1b804/?hmac=b2a4123d90f8d4ed00c580a671bd2936fe9acf214ac0d54dea751040149c0f26&shop=arheo.myshopify.com&state=163181900538700&timestamp=1631819064";
+let authenticateUrl = "https://api.thecodemesh.online/api/v1/enable/user-workflow/614393c4179d450012f1b804/?";
 const enableUrl = "https://api.thecodemesh.online/api/v1/enable/enable-user-workflow/614393c4179d450012f1b804/?username=arheo.myshopify.com"
 
 function App() {
   useEffect(() => {
-    //authenticate();
+    
     console.log(window.location.search)
+    authenticateUrl += window.location.search;
+    authenticate();
   }, []);
  
   const authenticate = async () => {
