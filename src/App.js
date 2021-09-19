a59e60fd0e30e5c1106a81f25e270fd7ef78765c
Arheo8 <78500348+Arheo8@users.noreply.github.com>
2021-09-19 12:10:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,31 +5,24 @@ let authenticateUrl = "https://api.thecodemesh.online/api/v1/enable/user-workflo
 const enableUrl = "https://api.thecodemesh.online/api/v1/enable/enable-user-workflow/614393c4179d450012f1b804/?username=arheo.myshopify.com"
 
 function App() {
-
-  let message = "";
-  
   useEffect(() => {
     console.log(window.location.search)
     authenticateUrl += window.location.search;
-    const authenticate = async () => {
-      fetch(authenticateUrl)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-       if(data.message.authStatus.status===401)
-       {
-         window.location = (data.message.authStatus.value)
-       }
-       else 
-       {
-          message = "App Authorization Sucessfull."
-       }
-      });
-  
-    };
     authenticate();
   }, []);
-   
+ 
+  const authenticate = async () => {
+    fetch(authenticateUrl)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+     if(data.message.authStatus.status===401)
+     {
+       window.location = (data.message.authStatus.value)
+     }
+    });
+
+  };
 
   const enableWorkflow = async () => {
     fetch(enableUrl)
@@ -43,8 +36,7 @@ function App() {
     <div className="App">
       <div>
         <h1>Sync_Customer_App</h1>
-        <h2>{message}</h2>
-        <Button name="Sync" click={enableWorkflow} />      
+        <Button name="Sync" click={enableWorkflow} />        
       </div>
     </div>
   );
