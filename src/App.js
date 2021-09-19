e689cfe103f65b0ba626acb0cde84495f3b24a35
Arheo8 <78500348+Arheo8@users.noreply.github.com>
2021-09-19 11:57:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,31 +5,31 @@ let authenticateUrl = "https://api.thecodemesh.online/api/v1/enable/user-workflo
 const enableUrl = "https://api.thecodemesh.online/api/v1/enable/enable-user-workflow/614393c4179d450012f1b804/?username=arheo.myshopify.com"
 
 function App() {
+
+  let verify = false;
+  
   useEffect(() => {
     console.log(window.location.search)
     authenticateUrl += window.location.search;
+    const authenticate = async () => {
+      fetch(authenticateUrl)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+       if(data.message.authStatus.status===401)
+       {
+         window.location = (data.message.authStatus.value)
+       }
+       else 
+       {
+          verify = true;
+       }
+      });
+  
+    };
     authenticate();
   }, []);
-  
-
-  let verify = false;
-
-  const authenticate = async () => {
-    fetch(authenticateUrl)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-     if(data.message.authStatus.status===401)
-     {
-       window.location = (data.message.authStatus.value)
-     }
-     else 
-     {
-        verify = true;
-     }
-    });
-
-  };
+   
 
   const enableWorkflow = async () => {
     fetch(enableUrl)
